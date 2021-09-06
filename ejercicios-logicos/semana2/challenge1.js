ca6454eabad7311260cf91ee0b210efcf35d4078
Leonardo Lugo <leolugo5@outlook.com>
2021-09-06 11:52:12 -0500

    New
    hope

--- ejercicios-logicos/semana2/challenge1.js
+++ ejercicios-logicos/semana2/challenge1.js
@@ -1,13 +1,12 @@
 function twoSums(numbs, target) {
-    for (let i = 0; i < numbs.length; i++) {
-        for (let j = 1; j < numbs.length; j++) {    
-            if ((numbs[i] + numbs[j]) === target) {
+    for (let i = 0; i < numbs.length; i ++) {
+        for (let j = 0; j < numbs.length; j ++) {
+            if ((i != j) && (numbs[i] + numbs[j]) == target) {
                 const res = [numbs.indexOf(numbs[i]), numbs.indexOf(numbs[j])]
                 return res
-            }        
+            }
         }
     }
-     
 }
 
 module.exports = twoSums;
